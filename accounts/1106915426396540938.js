3d47482a96d48415d3ed7752bcc76f10cb0b4bf4
Dexter <65877374+Oxuu1@users.noreply.github.com>
2023-07-01 18:08:10 +0100

    Update 1106915426396540938.js

--- accounts/1106915426396540938.js
+++ accounts/1106915426396540938.js
@@ -1,3 +1,1 @@
-const account = { username: 'Dexterj1', password: 'sdfdsf' };
-const account = { username: 'Rily', password: 'Spiderman99.' };
-const account = { username: 'Francuim', password: 'Spiderman99.' };
+
